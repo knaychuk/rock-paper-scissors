7e332da7a3917d3a249d4e571dcd5d068a2ac0ef
knaychuk <knaychuk@gmail.com>
2022-03-23 10:51:43 -0700

    working score

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
+const userScore = document.getElementById('user-score');
+const computerScore = document.getElementById('computer-score');
 
 function getComputerChoice() {
     const choices = ['r', 'p', 's'];
@@ -21,6 +23,8 @@ scissors.addEventListener("click", function() {
     compare("s");
 });
 
+let uScore = 0;
+let cScore = 0;
 
 function compare(choice) {
     const computerChoice = getComputerChoice();
@@ -30,12 +34,20 @@ function compare(choice) {
     if(userChoice === computerChoice)  {
         console.log("its a draw");
     } else if (userChoice === 'r' && computerChoice === 's') {
+        uScore++;
+        userScore.innerHTML = uScore;
         console.log("you win");
     } else if (userChoice === 'p' && computerChoice === 'r') {
+        uScore++;
+        userScore.innerHTML = uScore;
         console.log("you win");
     } else if (userChoice === 's' && computerChoice === 'p') {
+        uScore++;
+        userScore.innerHTML = uScore;
         console.log("you win");
     } else {
+        cScore++;
+        computerScore.innerHTML = cScore;
         console.log("you lose");
     }
 
@@ -43,3 +55,8 @@ function compare(choice) {
 
 
 
+
+// function change() {
+//     score++;
+//     userScore.innerHTML = score;
+// }
